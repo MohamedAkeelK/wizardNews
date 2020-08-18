5f9caf7761142670d99a714a703cdbfe1e56c05f
unknown <Mohamedakeelkhan@gmail.com>
2020-08-18 14:07:03 -0400

    sec commit

--- Wizard-news-pt1/app.js
+++ Wizard-news-pt1/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  //get list of posts
+  const posts = postBank.list();
+  const fi = postBank.find();
+
+  //prepare html to send as output
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => '<li>${postBank.title}</li>').join('')}
+    </ul>
+  </body>
+  </html>`;
+
+  //send a response
+  res.send(html)
+});
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
